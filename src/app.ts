4fa17747d9a421661592f03a90ff6dfd4de31d1b
diamkil <35732047+diamkil@users.noreply.github.com>
2021-07-29 15:40:29 -0400

    Basic functionality

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,44 @@
-import Express from 'express'
+import express from 'express';
+const minify = require('express-minify-html-2');
+import getImageInfo from './lib/getImageInfo';
 
-const app = Express()
+const app = express();
+const port = process.env.PORT || 8757;
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: false }));
+app.use(
+  minify({
+    override: true,
+    exception_url: false,
+    htmlMinifier: {
+      removeComments: true,
+      collapseWhitespace: true,
+      collapseBooleanAttributes: true,
+      removeAttributeQuotes: true,
+      removeEmptyAttributes: true,
+      minifyJS: true,
+    },
+  }),
+);
 
 app.get('/', (req, res) => {
-    res.send("Hello World!")
-})
+  res.render('../views/index.ejs', {
+    title: "diamkil's IMG Share",
+  });
+});
 
-app.listen(3000)
+app.get('/:time', (req, res) => {
+  const imageInfo = getImageInfo(req.params.time);
+  res.render('../views/image.ejs', {
+    imageInfo: imageInfo,
+    title: "diamkil's IMG Share",
+  });
+});
+
+app.post('/addImage', (req, res) => {});
+
+app.listen(port, function () {
+  console.log(`Listening on http://localhost:${port}`);
+});
